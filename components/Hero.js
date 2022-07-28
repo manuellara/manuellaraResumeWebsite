063dc5421abe621d3a969c540fbef5d407aeab1a
Manuel Lara <manuellaraa@gmail.com>
2022-07-28 01:25:34 -0700

    updated icons

--- components/Hero.js
+++ components/Hero.js
@@ -51,6 +51,11 @@ export default function Hero() {
                 <svg className='w-20' viewBox="0 0 24 24">
                     <path fill="currentColor" d="M7 7H5A2 2 0 0 0 3 9V17H5V13H7V17H9V9A2 2 0 0 0 7 7M7 11H5V9H7M14 7H10V17H12V13H14A2 2 0 0 0 16 11V9A2 2 0 0 0 14 7M14 11H12V9H14M20 9V15H21V17H17V15H18V9H17V7H21V9Z" />
                 </svg>
+
+                {/* vs code */}
+                <svg className='w-20' viewBox="0 0 24 24">
+                    <path fill="currentColor" d="M17,8.5L12.25,12.32L17,16V8.5M4.7,18.4L2,16.7V7.7L5,6.7L9.3,10.03L18,2L22,4.5V20L17,22L9.34,14.66L4.7,18.4M5,14L6.86,12.28L5,10.5V14Z" />
+                </svg>
             </div>
 
             <div className='flex space-x-4'>
